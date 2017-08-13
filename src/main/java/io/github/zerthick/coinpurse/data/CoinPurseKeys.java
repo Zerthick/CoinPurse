586d9850cef9b2131092820c9792d2d410c890d4
Zerthick <zerthick@gmail.com>
2017-08-13 18:26:35 -0400

    Update Copyright

--- src/main/java/io/github/zerthick/coinpurse/data/CoinPurseKeys.java
+++ src/main/java/io/github/zerthick/coinpurse/data/CoinPurseKeys.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017  Zerthick
+ *
+ * This file is part of CoinPurse.
+ *
+ * CoinPurse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CoinPurse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ */
+
 package io.github.zerthick.coinpurse.data;
 
 import com.google.common.reflect.TypeToken;
@@ -5,8 +23,6 @@ import org.spongepowered.api.data.DataQuery;
 import org.spongepowered.api.data.key.Key;
 import org.spongepowered.api.data.key.KeyFactory;
 import org.spongepowered.api.data.value.mutable.MutableBoundedValue;
-
-import java.math.BigInteger;
 
 public class CoinPurseKeys {
 
--- src/main/java/io/github/zerthick/coinpurse/data/immutable/ImmutableCoinPurseData.java
+++ src/main/java/io/github/zerthick/coinpurse/data/immutable/ImmutableCoinPurseData.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2017  Zerthick
+ *
+ * This file is part of CoinPurse.
+ *
+ * CoinPurse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CoinPurse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ */
+
 package io.github.zerthick.coinpurse.data.immutable;
 
 import io.github.zerthick.coinpurse.data.CoinPurseKeys;
